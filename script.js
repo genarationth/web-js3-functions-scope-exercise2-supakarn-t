8814850efc2f665ead15a959aa1bfdb42bf0bcd2
Supakarn Tangsirivatthanavong <hikaru8889@gmail.com>
2024-05-14 14:22:27 +0700

    finish exercise

---
+++ script.js
@@ -0,0 +1,11 @@
+// Exercise 2.1 create a expression function for checking if a number is even
+const is_even_expression = function (num) {
+	return num % 2 === 0 ? "YES" : "NO";
+};
+console.log(is_even_expression(1));
+console.log(is_even_expression(2));
+
+// Exercise 2.2 converts the function from Exercise 2.1 into an arrow function
+const is_even_arrow = (num) => (num % 2 === 0 ? "YES" : "NO");
+console.log(is_even_arrow(1));
+console.log(is_even_arrow(2));
